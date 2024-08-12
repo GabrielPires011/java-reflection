57327bf1d5bb788ac17b4ef7f60a2cb09d1745ba
Gabriel Teixeira <pires.rocha011@gmail.com>
2024-08-12 12:46:39 -0300

    add deveriaLancarException and deveriaTransformarClasseModelParaRecordDtoComCampoNull

--- src/test/java/br/com/reflection/TransformatorTest.java
+++ src/test/java/br/com/reflection/TransformatorTest.java
@@ -5,20 +5,36 @@ import br.com.model.Usuario;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TransformatorTest {
 
+    private Usuario usuario = new Usuario(1L, "Teste", 12345678912L);
+
     @Test
     void deveriaTransformarClasseModelParaRecordDto() {
-        var usuario = new Usuario(1L, "Teste", 12345678912L);
-
-        var transformator = new Transformator();
-
-        DadosDetalhadosUsuarioDto dadosDetalhados = transformator.transformarModelEmRecordDto(usuario,
+        DadosDetalhadosUsuarioDto dadosDetalhados = new Transformator().transformarModelEmRecordDto(usuario,
                 "DadosDetalhadosUsuario");
 
         assertNotNull(dadosDetalhados);
         assertEquals(dadosDetalhados.nome(), usuario.getNome());
         assertEquals(dadosDetalhados.cpf(), usuario.getCpf());
+    }
+
+    @Test
+    void deveriaLancarException() {
+        assertThrows(RuntimeException.class, () -> new Transformator().transformarModelEmRecordDto(usuario,
+                null));
+    }
+
+    @Test
+    void deveriaTransformarClasseModelParaRecordDtoComCampoNull() {
+        var usuario = new Usuario(1L, "Teste", null);
+        DadosDetalhadosUsuarioDto dadosDetalhados = new Transformator().transformarModelEmRecordDto(usuario,
+                "DadosDetalhadosUsuario");
+
+        assertNotNull(dadosDetalhados);
+        assertEquals(dadosDetalhados.nome(), usuario.getNome());
+        assertNull(dadosDetalhados.cpf());
     }
 }
